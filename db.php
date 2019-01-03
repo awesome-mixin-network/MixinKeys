8cec7126dd39af02de1ebe7530ed578f60cd18e1
Иван <if1242@yandex.ru>
2019-01-03 21:46:26 +0300

    Update db.php

--- db.php
+++ db.php
@@ -4,11 +4,11 @@ include_once 'sql.php';
 $database = new Database();
 $db = $database->getConnection();
 /*define('DB_TYPE', 'mysql');
-define('DB_HOST', 'ouchsu00.mysql');
+define('DB_HOST', '****');
 define('DB_PORT', 3306);
-define('DB_NAME', 'ouchsu00_mixin');
-define('DB_USERNAME', 'ouchsu00_adm');
-define('DB_PASSWORD', 'FU/i3sBV');
+define('DB_NAME', '****');
+define('DB_USERNAME', '****');
+define('DB_PASSWORD', '****');
 
 $dsn = DB_TYPE . ':dbname=' . DB_NAME . ';host=' . DB_HOST . ';port=' . DB_PORT;
 
